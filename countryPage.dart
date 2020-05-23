30ae9e6247c1825301a7f6e23b899a808e30b72f
webdesigner7050 <63560450+webdesigner7050@users.noreply.github.com>
2020-05-23 23:36:12 +0530

    Add files via upload

---
+++ countryPage.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class CountryPage extends StatefulWidget {
+  @override
+  _CountryPageState createState() => _CountryPageState();
+}
+
+
+
+class _CountryPageState extends State<CountryPage> {
+
+  List countryData;
+  fetchCountryData() async{
+    http.Response response= await http.get('https://corona.lmao.ninja/v2/countries');
+    setState(() {
+      countryData=json.decode(response.body);
+    });
+  }
+
+
+  @override
+  void initState() {
+  fetchCountryData();
+    super.initState();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text('Country Stats'),),
+    body:countryData==null?Center(child: CircularProgressIndicator(),):
+    ListView.builder(itemBuilder: (context,index){
+      return Container(
+      height:130,
+        margin: EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+        decoration: BoxDecoration(color: Colors.white,boxShadow: [
+          BoxShadow(color: Colors.grey[100],blurRadius: 10,offset: Offset(0,10)),
+        ]),
+        child: Row(
+          children: <Widget>[
+         Container(
+           margin: EdgeInsets.symmetric(horizontal: 10),
+           child: Column(
+             mainAxisAlignment: MainAxisAlignment.center,
+             children: <Widget>[
+               Text(countryData[index]['country'],style: TextStyle(fontWeight: FontWeight.bold),),
+               Image.network(countryData[index]['countryInfo']['flag'],height: 70,width: 90,),
+
+             ],
+           ),
+         ),
+            Expanded(child:Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text('CONFIRMED:'+countryData[index]['cases'].toString(),style:TextStyle(fontWeight: FontWeight.bold,color: Colors.red),),
+                  Text('ACTIVE:'+countryData[index]['active'].toString(),style:TextStyle(fontWeight: FontWeight.bold,color: Colors.green),),
+                  Text('RECOVERED:'+countryData[index]['recovered'].toString(),style:TextStyle(fontWeight: FontWeight.bold,color: Colors.blue),),
+                  Text('DEATHS:'+countryData[index]['deaths'].toString(),style:TextStyle(fontWeight: FontWeight.bold,color: Colors.grey[700]),),
+                ],
+              ),
+
+            ),),
+          ],
+        ),
+      );
+    },
+      itemCount: countryData == null ? 0 : countryData.length ,
+    ),
+    ) ;
+
+  }
+}
+
